c78cefa9d9733581659cc874a90a189646c69502
Azkka azeem <azkaazeem804@gmail.com>
2026-05-20 09:59:22 +0500

    env error

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,4 +1,4 @@
-import cloudinary from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -14,9 +14,9 @@ cloudinary.config({
 const uploadImg = async (file) => {
   try {
     const fileName = `blog-${file.originalname.split(".")[0]}_${Date.now()}`;
-    
+
     const uploadStr = await new Promise((resolve, reject) => {
-      const uploadd = cloudinary.v2.uploader.upload_stream(
+      const uploadd = cloudinary.uploader.upload_stream(
         {
           public_id: fileName,
           folder: "blogs",
@@ -26,33 +26,28 @@ const uploadImg = async (file) => {
           resource_type: "auto",
         },
         (err, result) => {
-          if (err) {
-            console.log('err', err);
-            return reject(err);
-          }
-          if (result) {
-            resolve(result);
-          }
+          if (err) return reject(err);
+          resolve(result);
         }
       );
       uploadd.end(file.buffer);
     });
-    
+
     return { image: uploadStr.secure_url, public_id: uploadStr.public_id };
   } catch (error) {
-    console.log('error-->', error);
+    throw error;
   }
 };
 
 // Function to delete image
 const deleteImg = async (public_id) => {
   try {
-    const result = await cloudinary.v2.uploader.destroy(public_id, {
+    const result = await cloudinary.uploader.destroy(public_id, {
       resource_type: "image"
     });
     return result;
   } catch (error) {
-    console.log('error in dlt-->', error);
+    throw error;
   }
 };
 
